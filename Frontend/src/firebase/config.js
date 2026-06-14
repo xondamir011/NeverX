bd35e8d07fdbf2146ab6acdde0587bb42e6b6327
xondamir011 <xondamirmadaliyev79@gmail.com>
2026-06-14 22:04:38 +0500

    configjs

--- Frontend/src/firebase/config.js
+++ Frontend/src/firebase/config.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnJCaACKYimBbKX8Hsb0j5Cmgt958zrPA",
-  authDomain: "mongodb-af2aa.firebaseapp.com",
-  projectId: "mongodb-af2aa",
-  storageBucket: "mongodb-af2aa.appspot.com",
-  messagingSenderId: "753459863047",
-  appId: "1:753459863047:web:8736ac7c2978ed375814a2"
+  apiKey: "AIzaSyDjjpuUTVR-huy5OCdYSsGLQJb1ZGK_R7Y",
+  authDomain: "neverx-1bc28.firebaseapp.com",
+  projectId: "neverx-1bc28",
+  storageBucket: "neverx-1bc28.firebasestorage.app",
+  messagingSenderId: "812758745661",
+  appId: "1:812758745661:web:82e9b65cd411d6e1122054",
+  measurementId: "G-VPJHV7L068"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
